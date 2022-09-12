318bbc36e2cb4c62974c42008eefb8826f2da51c
Gra1988 <95895591+Gra1988@users.noreply.github.com>
2022-09-11 22:31:57 -0300

    correcion error en educacion

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion.model';
   providedIn: 'root'
 })
 export class EducacionService {
-  URL = 'https://app-backgsc.herokuapp.com/educacion';
+  URL = 'https://app-backgsc.herokuapp.com/educacion/';
 
   constructor(private http: HttpClient) { }
   
